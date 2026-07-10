19b63dd089583f518e5f44b3ae43daa6cf2b601a
Your Name <230398863@mycput.ac.za>
2026-07-10 17:51:52 +0200

    Implement ClinicStaff repository and service

--- src/main/java/za/ac/cput/service/impl/IClinicStaffService.java
+++ src/main/java/za/ac/cput/service/impl/IClinicStaffService.java
@@ -1,4 +1,24 @@
 package za.ac.cput.service.impl;
 
+import za.ac.cput.domain.user.ClinicStaff;
+
+import java.util.List;
+
 public interface IClinicStaffService {
+
+    ClinicStaff create(ClinicStaff clinicStaff);
+
+    ClinicStaff read(Integer id);
+
+    ClinicStaff update(ClinicStaff clinicStaff);
+
+    void delete(Integer id);
+
+    List<ClinicStaff> getAll();
+
+    ClinicStaff findByEmail(String email);
+
+    List<ClinicStaff> findByDepartment(String department);
+
+    List<ClinicStaff> findByStaffRole(String staffRole);
 }
